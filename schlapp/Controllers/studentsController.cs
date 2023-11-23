2c115d259ec75f3611a98fffbadb106a4a7ca1c8
sivasai-jayanthi <sai89938@gmail.com>
2023-11-23 17:51:56 +0530

    Add project files.

---
+++ schlapp/Controllers/studentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using schlapp;
+
+namespace schlapp.Controllers
+{
+    public class studentsController : Controller
+    {
+        private schooldbsEntities db = new schooldbsEntities();
+
+        // GET: students
+        public ActionResult Index()
+        {
+            return View(db.students.ToList());
+        }
+
+        // GET: students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,address,email")] student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,address,email")] student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            student student = db.students.Find(id);
+            db.students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ schlapp/Controllers/subjectsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using schlapp;
+
+namespace schlapp.Controllers
+{
+    public class subjectsController : Controller
+    {
+        private schooldbsEntities db = new schooldbsEntities();
+
+        // GET: subjects
+        public ActionResult Index()
+        {
+            return View(db.subjects.ToList());
+        }
+
+        // GET: subjects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // GET: subjects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: subjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name")] subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.subjects.Add(subject);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(subject);
+        }
+
+        // GET: subjects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: subjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name")] subject subject)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subject).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(subject);
+        }
+
+        // GET: subjects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            subject subject = db.subjects.Find(id);
+            if (subject == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subject);
+        }
+
+        // POST: subjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            subject subject = db.subjects.Find(id);
+            db.subjects.Remove(subject);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
